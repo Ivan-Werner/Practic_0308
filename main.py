e7186a4fd1f142eabea3429b8542fd99858e0bb0
Ivan_K <ikalinov1989yandex.ru>
2024-08-03 12:28:24 +0400

    add substract function

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 def sum_number(a, b):
     return a + b
 
+def substract_number(a, b):
+    return a - b
+
 
 if __name__ == '__main__':
     print(sum_number(2, 3))
+    print(substract_number(2, 3))
     
